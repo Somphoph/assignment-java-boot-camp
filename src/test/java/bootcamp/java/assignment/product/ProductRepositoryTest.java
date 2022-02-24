4cfd6a4fb443262f3e330b33e2fa9239d3c0c44f
somphop.h <somphop.h@outlook.com>
2022-02-24 20:48:59 +0700

    #2 - Update displayname on testcase in ProductRepositoryTest

--- src/test/java/bootcamp/java/assignment/product/ProductRepositoryTest.java
+++ src/test/java/bootcamp/java/assignment/product/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package bootcamp.java.assignment.product;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,6 +16,7 @@ class ProductRepositoryTest {
     private ProductRepository productRepository;
 
     @Test
+    @DisplayName("เมื่อใส่ Keyword ค้นหาด้วยคำที่มีคำนั้นอยู่ในชื่อของสินค้าต้องพบข้อมูล")
     void findByProductNameContainsIgnoreCase_with_keyword_found() {
         Product product = new Product(6, "Dell UltraSharp 43 4K USB-C Monitor U4320Q Monitor 4K UHD 3840 x2160 60Hz Warranty 3 Year 96% sRGB", "Dell", 32390.00, 32390.00, 0, null, 0, 5);
         productRepository.save(product);
@@ -23,6 +25,7 @@ class ProductRepositoryTest {
     }
 
     @Test
+    @DisplayName("เมื่อใส่ Keyword ค้นหาด้วยคำที่ไม่มีคำนั้นอยู่ในชื่อของสินค้าต้องไม่พบข้อมูล")
     void findByProductNameContainsIgnoreCase_with_keyword_notfound() {
         Product product = new Product(6, "Dell UltraSharp 43 4K USB-C Monitor U4320Q Monitor 4K UHD 3840 x2160 60Hz Warranty 3 Year 96% sRGB", "Dell", 32390.00, 32390.00, 0, null, 0, 5);
         productRepository.save(product);
