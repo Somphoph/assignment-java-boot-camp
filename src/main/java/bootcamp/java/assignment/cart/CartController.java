26cba855fb68f681981ce8fa021bee0aa5c2ae5b
somphop.h <somphop.h@outlook.com>
2022-02-27 20:46:46 +0700

    #7 - Add CartController

--- src/main/java/bootcamp/java/assignment/cart/CartController.java
+++ src/main/java/bootcamp/java/assignment/cart/CartController.java
@@ -7,14 +7,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class CartController {
     private final CartService cartService;
+    private final CartMapper cartMapper;
 
-    public CartController(CartService cartService) {
+    public CartController(CartService cartService, CartMapper cartMapper) {
         this.cartService = cartService;
+        this.cartMapper = cartMapper;
     }
 
     @PostMapping
     public DefaultResponse addToCart(@RequestBody AddToCartRequest addToCartRequest, @PathVariable int userId) {
         cartService.add(addToCartRequest.getProductId(), addToCartRequest.getAmount(), userId);
         return new DefaultResponse("success");
+    }
+
+    @GetMapping
+    public CartResponse getCart(@PathVariable int userId) {
+        Cart cart = cartService.findCartByUserId(userId);
+        return cartMapper.cartToCartResponse(cart);
     }
 }
