75369c17319be0c7932277202d9dd109a0b11e7b
somphop.h <somphop.h@outlook.com>
2022-03-09 08:32:20 +0700

    #9 - Delete AddressMapper Interface

--- src/main/java/bootcamp/java/assignment/address/AddressMapper.java
+++ src/main/java/bootcamp/java/assignment/address/AddressMapper.java
@@ -1,9 +1,28 @@
 package bootcamp.java.assignment.address;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public interface AddressMapper {
-    List<AddressResponse> addressesToAddressesResponse(List<Address> addrs);
+@Component
+public class AddressMapper {
+    public List<AddressResponse> addressesToAddressesResponse(List<Address> addrs) {
+        List<AddressResponse> responses = new ArrayList<>();
+        for (Address addr : addrs) {
+            responses.add(addressToAddressResponse(addr));
+        }
+        return responses;
+    }
 
-    AddressResponse addressToAddressResponse(Address addr);
+    public AddressResponse addressToAddressResponse(Address addr) {
+        AddressResponse addrResp = new AddressResponse();
+        addrResp.setAddress1(addr.getAddress1());
+        addrResp.setAddress2(addr.getAddress2());
+        addrResp.setDistrict(addr.getDistrict());
+        addrResp.setPostCode(addr.getPostCode());
+        addrResp.setDefault(addr.isDefault());
+
+        return addrResp;
+    }
 }
