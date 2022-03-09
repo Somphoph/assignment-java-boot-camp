6892281fed74d988f92b0297daa4b9a93ea8e055
somphop.h <somphop.h@outlook.com>
2022-03-09 08:33:10 +0700

    #9 - Delete ProductService Interface

--- src/main/java/bootcamp/java/assignment/product/ProductService.java
+++ src/main/java/bootcamp/java/assignment/product/ProductService.java
@@ -1,11 +1,33 @@
 package bootcamp.java.assignment.product;
 
 import bootcamp.java.assignment.product.exception.ProductNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface ProductService {
-    List<Product> findProducts(String keyword);
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
 
-    Product findProductById(int productId) throws ProductNotFoundException;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findProducts(String keyword) {
+        if (keyword != null) {
+            return productRepository.findByProductNameContainsIgnoreCase(keyword.toLowerCase());
+        } else {
+            return productRepository.findAll();
+        }
+    }
+
+    public Product findProductById(int productId) throws ProductNotFoundException {
+        Optional<Product> result = productRepository.findById(productId);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new ProductNotFoundException();
+        }
+    }
 }
